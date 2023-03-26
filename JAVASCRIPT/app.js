e5c43cf20fc8f5cc06bb4bfbd924e3aa22c5de63
Casper Hexel <hexelcasper333@gmail.com>
2023-03-26 19:57:15 +0200

    Navbar Update

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -13,7 +13,10 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`
             }
         });
-    })
+
+        burger.classList.toggle('toggle');
+
+    });
     
 }
 
